491795251c18e82c47dff46639b717da71d7016e
AlexDev04 <Web.Yakovlev04@yandex.ru>
2022-04-26 22:41:19 +0300

    Archive clear fix
    добавлена подгрузка карточек после удаления содержимого архива

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { AppRoute } from '../../const';
 import { NavLink, useLocation, Link } from 'react-router-dom';
 import { clearArchive } from '../../API';
+import { events } from '../../store';
 
 export const Header = () => {
 
@@ -9,6 +10,7 @@ export const Header = () => {
 
     const handleDelete = () => {
         clearArchive();
+        events.fetch();
     }
 
     return (
@@ -18,12 +20,14 @@ export const Header = () => {
                 <div className="main__header-group-lnk">
                     <NavLink to="/" className={`main__header-lnk ${pathname === AppRoute.MAIN && 'lnk-active'}`}>События</NavLink>
                     <NavLink to="/archive" className={`main__header-lnk ${pathname === AppRoute.ARCHIVE && 'lnk-active'}`}>Архив</NavLink>
-                    <button
-                        className="main__header-del"
-                        onClick={handleDelete}
-                    >
-                        Удалить все
-                    </button>
+                    {pathname === AppRoute.ARCHIVE && 
+                        <button
+                            className="main__header-del"
+                            onClick={handleDelete}
+                        >
+                            Удалить все
+                        </button>
+                    }
                 </div>
             </section>
         </section>
